0ee514665a35bbc2b9b9b7dc7201a68a279197cd
Jukka Aittokallio <jukka@aittokallio.com>
2019-01-20 10:39:22 +0200

    Simplified code a bit. Changed test runner from jest to tape

--- src/index.js
+++ src/index.js
@@ -1,83 +1,79 @@
-const EFF_TAG = () => EFF_TAG;
+const EFF_TAG = Symbol();
 
 function EXECUTE_BIND(eff, next) {
-    return run(eff).then(a => {
-        const nextEffect = next(a);
-        if (!isEffect(nextEffect)) {
-            return nextEffect;
-        } else {
-            return run(nextEffect);
-        }
-    });
+  return run(eff).then(a => {
+    const nextEffect = next(a);
+    if (isEffect(nextEffect)) {
+      return run(nextEffect);
+    } else {
+      return nextEffect;
+    }
+  });
 }
 
 function EXECUTE_UNIT(v) {
-    return Promise.resolve(v);
+  return Promise.resolve(v);
+}
+
+function make(obj) {
+  return { ...obj, _ef_tag_: EFF_TAG };
 }
 
 function isEffect(value) {
-    return typeof value === 'object' && 
-           value._ef_tag_ === EFF_TAG,
-           typeof value.executor === 'function' && 
-           Array.isArray(value.params);
-}
-
-function willExecute(eff, f) {
-    return eff.executor === f;
-}
-
-function getParams(eff) {
-    return eff.params;
+  return (
+    typeof value === "object" && value._ef_tag_ === EFF_TAG,
+    typeof value.executor === "function" && Array.isArray(value.params)
+  );
 }
 
 function isUnit(eff) {
-    return willExecute(eff, EXECUTE_UNIT);
+  return eff.executor === EXECUTE_UNIT;
 }
 
 function isBind(eff) {
-    return willExecute(eff, EXECUTE_BIND);
+  return eff.executor === EXECUTE_BIND;
 }
 
 function eff(executor, ...params) {
-    return { executor, params, _ef_tag_: EFF_TAG };
+  return make({ executor, params });
 }
 
 function bind(eff, next) {
-    return { executor: EXECUTE_BIND, params: [ eff, next ] }
+  return make({ executor: EXECUTE_BIND, params: [eff, next] });
 }
 
 function unit(value) {
-    return { executor: EXECUTE_UNIT, params: [ value ] };
+  return make({ executor: EXECUTE_UNIT, params: [value] });
 }
 
 function run(eff) {
-    if (!isEffect(eff)) {
-        throw new Error("Invalid effect given to Eff.run. Did you create the effect with Eff.eff?");
-    }
-    return Promise.resolve(eff.executor.call(null, ...eff.params));
+  if (!isEffect(eff)) {
+    throw new Error(
+      "Invalid effect given to Eff.run. Did you create the effect with Eff.eff?"
+    );
+  }
+  return Promise.resolve(eff.executor.call(null, ...eff.params));
 }
 
 function simulate(eff, mocks, offset = 0, next = undefined) {
-    if (isBind(eff)) {
-        return simulate(eff.params[0], mocks, offset, eff.params[1]);
-    }
+  if (isBind(eff)) {
+    return simulate(eff.params[0], mocks, offset, eff.params[1]);
+  }
 
-    return Promise.resolve(mocks[offset].call(null, eff)).then(
-        result => next
-                  ? simulate(next(result), mocks, offset+1, null)
-                  : Promise.resolve(result)
-    );
+  return Promise.resolve(mocks[offset].call(null, ...eff.params)).then(result =>
+    next
+      ? simulate(next(result), mocks, offset + 1, null)
+      : Promise.resolve(result)
+  );
 }
 
 module.exports = {
-    isEffect,
-    isBind,
-    isUnit,
-    willExecute,
-    getParams,
-    eff,
-    bind,
-    unit,
-    run,
-    simulate
+  isEffect,
+  isBind,
+  isUnit,
+  eff,
+  bind,
+  unit,
+  run,
+  simulate
 };
--- tests/eff.test.js
+++ tests/eff.test.js
@@ -1,99 +1,129 @@
-const Eff = require('../src/index');
+const test = require("blue-tape");
+const Eff = require("../src/index");
 
-test("eff-function creates an effect object", () => {
-    const f = () => 'foo';
-    const res = Eff.eff(f, 1, 2);
-    expect(Eff.willExecute(res, f)).toBeTruthy();
-    expect(Eff.getParams(res)).toEqual([1, 2]);
+test("eff-function creates an effect object", t => {
+  t.plan(2);
+  const f = () => "foo";
+  const res = Eff.eff(f, 1, 2);
+  t.equal(res.executor, f, "with correct executor");
+  t.deepEqual(res.params, [1, 2], "with correct parameters");
 });
 
-test("run-function runs an effect as a promise", () => {
-    const f = (a, b) => a + b;
-    const eff = Eff.eff(f, 'foo', 'bar');
-    return expect(Eff.run(eff)).resolves.toEqual("foobar");
+test("run-function runs an effect as a promise", t => {
+  t.plan(1);
+  const f = (a, b) => a + b;
+  const eff = Eff.eff(f, "foo", "bar");
+  return Eff.run(eff).then(actual =>
+    t.equal(actual, "foobar", "with correct result")
+  );
 });
 
-test("running unit returns the contained value", () => {
-    return expect(Eff.run(Eff.unit('yeah'))).resolves.toEqual('yeah');
+test("running unit", t => {
+  return Eff.run(Eff.unit("yeah")).then(actual =>
+    t.equal(actual, "yeah", "returns the contained value")
+  );
 });
 
-test("binding passes return value to next function", () => {
-    const eff = Eff.bind(Eff.unit('yeah'), (v) => Eff.unit(v));
-    return expect(Eff.run(eff)).resolves.toEqual('yeah');
+test("binding passes return value to next function", t => {
+  const eff = Eff.bind(Eff.unit("yeah"), v => Eff.unit(v));
+  return Eff.run(eff).then(actual => t.equal(actual, "yeah", "correct result"));
 });
 
-test("bind works as expected", () => {
+test("bind with unit chain", t => {
+  const read = filename => Eff.unit("read:" + filename);
+  const write = data => Eff.unit("wrote:" + data);
 
-    const read = (filename) => Eff.unit('read:'+filename);
-    const write = (data) => Eff.unit('wrote:'+data);
-
-    const eff = Eff.bind(read('myfile'), write)
-    return expect(Eff.run(eff)).resolves.toEqual('wrote:read:myfile');
+  const eff = Eff.bind(read("myfile"), write);
+  return Eff.run(eff).then(actual =>
+    t.equal(actual, "wrote:read:myfile", "gives correct result")
+  );
 });
 
-test("bind works with a longer chain", () => {
-    const f1 = (a) => Eff.unit('F1:'+a);
-    const f2 = (a) => Eff.unit('F2:'+a);
-    const f3 = (a) => Eff.unit('F3:'+a);
-    const eff = Eff.bind(f1('start'), (r1) => 
-                Eff.bind(f2(r1), (r2) =>
-                Eff.bind(f3(r2), (r3) => 
-                Eff.unit(r3))));
-    return expect(Eff.run(eff)).resolves.toEqual('F3:F2:F1:start');
+test("bind with longer chain", t => {
+  const f1 = a => Eff.unit("F1:" + a);
+  const f2 = a => Eff.unit("F2:" + a);
+  const f3 = a => Eff.unit("F3:" + a);
+  const eff = Eff.bind(f1("start"), r1 =>
+    Eff.bind(f2(r1), r2 => Eff.bind(f3(r2), r3 => Eff.unit(r3)))
+  );
+  return Eff.run(eff).then(actual =>
+    t.equal(actual, "F3:F2:F1:start", "gives correct result")
+  );
 });
 
-test("simulate unit", () => {
-    let called = false;
-    const eff = Eff.unit('foo');
-    return Eff.simulate(eff, [
-        (eff) => {
-            expect(Eff.getParams(eff)).toEqual(['foo']);
-            called = true
-        }
-    ]).then(r => {
-        expect(called).toBeTruthy();
-    });
+test("simulate unit", t => {
+  let called = false;
+  const eff = Eff.unit("foo");
+  return Eff.simulate(eff, [
+    param => {
+      t.equal(param, "foo", "correct paramter to mock");
+      called = true;
+    }
+  ]).then(r => {
+    t.equal(called, true, "mock has been called");
+  });
 });
 
-test("simulate unit with bind", () => {
-    let called1 = false, called2 = false;
-    const eff = Eff.bind(Eff.unit('foo'), (x) => Eff.unit('bar'));
-    return Eff.simulate(eff, [
-        (eff) => {
-            expect(Eff.getParams(eff)).toEqual(['foo']);
-            called1 = true;
+test("simulate unit with bind", t => {
+  let called = 0;
+
+  const eff = Eff.bind(Eff.unit("foo"), x => Eff.unit("bar"));
+  return Eff.simulate(eff, [
+    param => {
+      t.equal(param, "foo", "param to first mock");
+      called++;
+    },
+    param => {
+      t.equal(param, "bar", "param to second mock");
+      called++;
+    }
+  ]).then(r => {
+    t.equal(called, 2, "Both mocks called");
+  });
+});
+
+test("simulate more complex chains", t => {
+  const getPerson = id => Eff.eff("getPersonIO", id);
+  const getCompany = id => Eff.eff("getCompanyIO", id);
+  const getPersonWithCompany = personId =>
+    Eff.bind(getPerson(personId), p =>
+      Eff.bind(getCompany(p.companyId), c =>
+        Eff.unit({ ...p, companyName: c.name })
+      )
+    );
+
+  const PersonId = 100;
+  const PersonName = "John";
+  const CompanyId = 200;
+  const CompanyName = "Acme Inc";
+
+  let called = 0;
+
+  return Eff.simulate(getPersonWithCompany(PersonId), [
+    id => {
+      t.equal(id, PersonId, "Param to first mock");
+      called++;
+      return { id, name: PersonName, companyId: CompanyId };
+    },
+    companyId => {
+      t.equal(companyId, CompanyId, "Param to second mock");
+      called++;
+      return { id: companyId, name: CompanyName };
+    },
+    person => {
+      t.deepEqual(
+        person,
+        {
+          id: PersonId,
+          name: PersonName,
+          companyId: CompanyId,
+          companyName: CompanyName
         },
-        (eff) => {
-            expect(Eff.getParams(eff)).toEqual(['bar']);
-            called2 = true;
-        },
-    ]).then(r => {
-        expect(called1).toBeTruthy();
-        expect(called2).toBeTruthy();
-    })
-
+        "Param to third mock"
+      );
+      called++;
+    }
+  ]).then(() => {
+    t.equal(called, 3, "All mocks called");
+  });
 });
-
-test("simulating mode complex function chains", () => {
-
-    const JOHN_ID = 1;
-    const ACME_ID = 1;
-    const WIDGET_ID = 2;
-
-    const getPerson = (id) => id === JOHN_ID ? Eff.unit({ id, name: 'John', companyId: ACME_ID }) : Eff.unit({ id, name: 'Mary', companyId: WIDGET_ID });
-    const getCompany = (id) => id === ACME_ID ? Eff.unit({ id, name: 'Acme Inc' }) : Eff.unit({ id, name: 'Widget Ltd' });
-
-    const getPersonWithCompany = (personId) =>
-        Eff.bind(getPerson(1), (p) => 
-        Eff.bind(getCompany(p.companyId), (c) => 
-        Eff.unit({ ...p, companyName: c.name })));
-
-    return Eff.simulate(getPersonWithCompany(1), [
-        () => ({ companyId: 1000 }),
-        () => ({ name: 'foobar' }),
-        (eff) => {
-            expect(Eff.getParams(eff)).toEqual([{ companyId: 1000, companyName: 'foobar' }]);
-        }
-    ]);
-});
-
